94992854c0eca8f4f791d63e521b9bbea460e92b
Erin Santosa <tun35949@temple.edu>
2023-09-26 11:45:15 -0400

    provided adapter parent in adapter implementation

--- app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
@@ -4,9 +4,10 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 // Step 3: Complete adapter implementation
+/* Step 3a: Provide Adapter Parent */
+class NumberDisplayAdapter (_numbers: Array<int>) : RecyclerView.Adapter<NumberDisplayAdapter.NumberViewHolder>() {
 
-class NumberDisplayAdapter /* Step 3a: Provide Adapter Parent */ {
-
+    private val numbers = _numbers
     class NumberViewHolder (val textView: TextView) : RecyclerView.ViewHolder (textView) {}
 
     //Step 3b: Complete function definitions for adapter
